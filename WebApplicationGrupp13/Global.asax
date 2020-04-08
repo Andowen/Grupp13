977520fff5fad3b78a70b97468320060f9699702
Andowen <46093390+Andowen@users.noreply.github.com>
2020-04-08 15:21:19 +0200

    Initial commit

---
+++ WebApplicationGrupp13/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationGrupp13.MvcApplication" Language="C#" %>
---
+++ WebApplicationGrupp13/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationGrupp13.Startup))]
+namespace WebApplicationGrupp13
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
