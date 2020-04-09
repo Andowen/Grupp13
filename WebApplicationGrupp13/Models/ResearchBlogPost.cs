8fdfc0e96d257b3d0cf0eedcbf014ea9988b8e44
Giullis <46340845+Giullis@users.noreply.github.com>
2020-04-09 13:23:50 +0200

    researchBlogPost

---
+++ WebApplicationGrupp13/Models/ResearchBlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ServiceStack.DataAnnotations;
+
+namespace WebApplicationGrupp13.Models
+{
+    public class ResearchBlogPost
+    {
+        [Key]
+        public int id { get; set; }
+        public string postText { get; set; }
+        public string title { get; set; }
+        public string date { get; set; }
+
+    }
+}
