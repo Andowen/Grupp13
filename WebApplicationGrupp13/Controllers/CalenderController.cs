f64263fdb2ffdca39eb65be459fbc30efb7eba69
afahlquist <alexandra.fahlquist@hotmail.se>
2020-04-14 10:53:09 +0200

    Skapat en kalender (CalenderController och Calender view)

--- WebApplicationGrupp13/Controllers/CalenderController.cs
+++ WebApplicationGrupp13/Controllers/CalenderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationGrupp13.Models;
 
 namespace WebApplicationGrupp13.Controllers
 {
@@ -12,6 +13,12 @@ namespace WebApplicationGrupp13.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult GetEvents() {
+            using (ApplicationDbContext dc = new ApplicationDbContext()) {
+                var events = dc.Calender.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
         }
     }
 }
