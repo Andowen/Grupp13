ae15d25f1df92d9c9dc291902fe9720f703607cd
Giullis <46340845+Giullis@users.noreply.github.com>
2020-04-15 13:51:29 +0200

    byggfel i formalblogpost pga  aspnetcore

--- WebApplicationGrupp13/Models/FormalBlogPost.cs
+++ WebApplicationGrupp13/Models/FormalBlogPost.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-//using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using ServiceStack.DataAnnotations;
 
 namespace WebApplicationGrupp13.Models {
