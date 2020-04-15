f633e841528be47b7ac26592ac96d231d95e422f
agnesskogfors <56914342+agnesskogfors@users.noreply.github.com>
2020-04-15 15:29:06 +0200

    Ändrat med den informella bloggen

--- WebApplicationGrupp13/Models/InformalBlogPost.cs
+++ WebApplicationGrupp13/Models/InformalBlogPost.cs
@@ -8,11 +8,15 @@ namespace WebApplicationGrupp13.Models {
     public class InformalBlogPost {
         [Key]
         public int id { get; set; }
+        [Display(Name = "Innehåll")]
         public string postText { get; set; }
+        [Display(Name = "Titel")]
         public string title { get; set; }
+        [Display(Name = "Författare")]
         public string creator { get; set; }
+        [Display(Name = "Datum")]
         public DateTime dateTime { get; set; }
-
+        [Display(Name = "Kategori")]
         public string category { get; set; }
 
     }
