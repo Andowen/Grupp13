abc3b7bc443691b133bdada9de4b29b07fd49f30
Giullis <46340845+Giullis@users.noreply.github.com>
2020-04-16 17:05:21 +0200

    meetingsklassen föll bort

--- WebApplicationGrupp13/Models/Meetings.cs
+++ WebApplicationGrupp13/Models/Meetings.cs
@@ -1,6 +1,22 @@
-﻿namespace WebApplicationGrupp13.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationGrupp13.Models
 {
     public class Meetings
     {
-    }
-}
+        [Key]
+        public string id { get; set; }
+        public string meetingName { get; set; }
+        public DateTime date1 { get; set; }
+
+        public DateTime date2 { get; set; }
+
+        public DateTime date3 { get; set; }
+
+        public string creator { get; set; }
+
+    } }
