cd63b5c7467c4a17f4dbbca499f4dbdd2b437f7c
Giullis <46340845+Giullis@users.noreply.github.com>
2020-04-16 16:52:28 +0200

    model meetings försvann

--- WebApplicationGrupp13/Models/Meetings.cs
+++ WebApplicationGrupp13/Models/Meetings.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
-namespace WebApplicationGrupp13.Models
+﻿namespace WebApplicationGrupp13.Models
 {
     public class Meetings
     {
-            [Key]
-            public int id { get; set; }
-            public string meetingName { get; set; }
-            public DateTime date1 { get; set; }
-            public DateTime date2 { get; set; }
-            public DateTime date3 { get; set; }
-            public int creator { get; set; }
-        }
+    }
 }
