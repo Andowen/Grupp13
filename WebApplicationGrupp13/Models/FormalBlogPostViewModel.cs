e747a63f7c8c55309a903fcabe5dd05ce6447586
tjenseen <tjenseen@hotmail.com>
2020-04-21 10:38:55 +0200

    KOMMENTARER TILL ALLAAAA

---
+++ WebApplicationGrupp13/Models/FormalBlogPostViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationGrupp13.Models {
+    public class FormalBlogPostViewModel {
+        public List<FormalBlogPost> formalBlogPosts { get; set; }
+        public List<FormalBlogPostComment> formalBlogPostComments { get; set; }
+    }
+}
