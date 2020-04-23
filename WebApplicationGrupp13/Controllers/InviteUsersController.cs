c86e7fb153f51a36ba2d92dd45062be2109e912d
afahlquist <alexandra.fahlquist@hotmail.se>
2020-04-23 10:59:20 +0200

    Lagt till InviteUserController, InviteUsermodel, InviteView

---
+++ WebApplicationGrupp13/Controllers/InviteUsersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationGrupp13.Models;
+using System.Text;
+
+namespace WebApplicationGrupp13.Controllers
+{
+    public class InviteUsersController : NotificationControllerBase
+    {
+        // GET: InviteUsers
+        [HttpGet]
+        public ActionResult InviteUsers()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<SelectListItem> listSelectListItems = new List<SelectListItem>();
+
+
+            foreach (ApplicationUser user in db.Users.ToList()) {
+                SelectListItem selectList = new SelectListItem() {
+                    Text = user.Firstname + " " + user.Lastname,
+                    Value = user.Id.ToString(),
+                    Selected = user.IsSelected
+                };
+                listSelectListItems.Add(selectList);
+            }
+
+            InviteUserModel inteviteUserModel = new InviteUserModel() {
+                InvitedUsers = listSelectListItems
+            };
+
+            return View(inteviteUserModel);
+        }
+        [HttpPost]
+        public string InviteUsers(IEnumerable<string> selectedUsers) {
+            if (selectedUsers == null) {
+                return "Ingen användare är vald.";
+            } else {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("You selected – " + string.Join(",", selectedUsers));
+                return sb.ToString();
+            }
+        }
+
+    }
+}
---
+++ WebApplicationGrupp13/Models/InviteUserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationGrupp13.Models {
+    public class InviteUserModel {
+        public IEnumerable<string> SelectedUsers { get; set; }
+        public IEnumerable<SelectListItem> InvitedUsers { get; set; }
+    }
+}
